55b3a4bd547557896871bb22f33d020fd15b1fc7
s-m-quadri <88645248+s-m-quadri@users.noreply.github.com>
2025-02-06 10:10:25 +0530

    Add deep clean

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 import re
 import tkinter as tk
-from tkinter import ttk, scrolledtext
+from tkinter import ttk, scrolledtext, PhotoImage
 import arxiv
 import threading
 import sys
 import os
+from PIL import Image, ImageTk
+
 
 def resource_path(relative_path):
-    """ Get the absolute path to the resource, works for dev and for PyInstaller """
-    if hasattr(sys, '_MEIPASS'):
+    """Get the absolute path to the resource, works for dev and for PyInstaller"""
+    if hasattr(sys, "_MEIPASS"):
         # PyInstaller creates a temp folder and stores files there
         return os.path.join(sys._MEIPASS, relative_path)
     return os.path.join(os.path.abspath("."), relative_path)
+
 
 # Function to perform the search
 def search_papers():
     # Disable the search button and show status
     search_button.config(state=tk.DISABLED, text="Searching...")
-    
+
     def perform_search():
         # Clear the results field
         result_field.delete(1.0, tk.END)
 
         # Get the query and result limit from the fields
         query = search_entry.get(1.0, tk.END).strip()
-        
+
         try:
             result_limit = int(result_limit_entry.get().strip())
         except ValueError:
@@ -42,7 +45,7 @@ def search_papers():
             search = arxiv.Search(
                 query=query,
                 max_results=result_limit,
-                sort_by=arxiv.SortCriterion.Relevance
+                sort_by=arxiv.SortCriterion.Relevance,
             )
 
             # Display the results
@@ -55,7 +58,9 @@ def search_papers():
                 result_field.insert(tk.END, f"{idx}. {title}\n")
                 if doi:
                     result_field.insert(tk.END, f"DOI: {doi}\n")
-                    result_field.insert(tk.END, f"{sci_hub_url}\n", ("link", sci_hub_url))
+                    result_field.insert(
+                        tk.END, f"{sci_hub_url}\n", ("link", sci_hub_url)
+                    )
                 result_field.insert(tk.END, f"{url}\n\n", ("link", url))
 
             # Make links clickable
@@ -69,9 +74,11 @@ def search_papers():
 
     threading.Thread(target=perform_search).start()
 
+
 # Function to open links
 def open_link(event):
     import webbrowser
+
     widget = event.widget
     index = widget.index("@%s,%s" % (event.x, event.y))
     tags = widget.tag_names(index)
@@ -80,70 +87,177 @@ def open_link(event):
             url = widget.get("%s linestart" % index, "%s lineend" % index).strip()
             webbrowser.open(url)
 
+
 # Function to clear input and output fields
 def clear_fields():
     search_entry.delete(1.0, tk.END)
     result_field.delete(1.0, tk.END)
-    
-    
-# Function to clean input
-# i.e. Remove non-alphanumeric characters (except spaces)
+
+
+# Function to clean input from special characters
 def clean_input():
     query = search_entry.get(1.0, tk.END).strip()
-    cleaned_query = re.sub(r'[^a-zA-Z0-9\s]', '', query)
+    cleaned_query = re.sub(r"[^a-zA-Z0-9\s]", "", query)
     search_entry.delete(1.0, tk.END)
     search_entry.insert(tk.END, cleaned_query)
 
 
-# Create the main window
+# Function to clean input from numbers as well
+def deep_clean_input():
+    query = search_entry.get(1.0, tk.END).strip()
+    cleaned_query = re.sub(r"[^a-zA-Z ]", "", query)
+    cleaned_query = re.sub(r"\s+", " ", cleaned_query)
+    cleaned_query = cleaned_query.strip().upper()
+    search_entry.delete(1.0, tk.END)
+    search_entry.insert(tk.END, cleaned_query)
+
+
+# Initialize the Tkinter application
 root = tk.Tk()
 root.iconbitmap(resource_path("icon.ico"))
 root.title("Search Research")
-root.geometry(f"750x600")
+root.geometry(f"800x800")
 
-# Set background color
+# Style the application
 root.configure(bg="#FEF9E1")
-
-# Add title and subtitle
-title_label = tk.Label(root, text="Search Research", font=("Arial", 32, "bold"), bg="#FEF9E1", fg="#6D2323")
-title_label.pack(pady=(35, 1))
-subtitle_label = tk.Label(root, text="Search for research papers with ease", font=("Arial", 12), bg="#FEF9E1", fg="#000")
-subtitle_label.pack(pady=(1, 15))
-
-# Create the multi-line text entry field
-search_label = tk.Label(root, text="Enter your search query:", bg="#FEF9E1", fg="#000")
-search_label.pack(pady=5)
-search_entry = scrolledtext.ScrolledText(root, wrap=tk.WORD, height=5, width=70, bg="#E5D0AC")
-search_entry.pack(pady=5)
-
-# Create the buttons and result limit input in a row
 style = ttk.Style()
 style.configure("Custom.TFrame", background="#FEF9E1")
-button_frame = ttk.Frame(root, style="Custom.TFrame")
-button_frame.pack(pady=10)
+image = ImageTk.PhotoImage(Image.open(resource_path("icon.ico")).resize((128, 128)))
 
-clear_input_button = tk.Button(button_frame, text="Clear Input", command=lambda: search_entry.delete(1.0, tk.END), bg="#6D2323", fg="#FEF9E1", font=("Arial", 10, "bold"))
-clear_input_button.grid(row=0, column=0, padx=5)
+# Create a canvas
+canvas = tk.Canvas(root)
+canvas.pack(side="left", fill="both", expand=True)
 
-clear_output_button = tk.Button(button_frame, text="Clear Output", command=lambda: result_field.delete(1.0, tk.END), bg="#6D2323", fg="#FEF9E1", font=("Arial", 10, "bold"))
-clear_output_button.grid(row=0, column=1, padx=5)
+# Create a scrollbar
+scrollbar = ttk.Scrollbar(root, orient="vertical", command=canvas.yview)
+scrollbar.pack(side="right", fill="y")
 
-result_limit_label = tk.Label(button_frame, text="Results Limit:", bg="#FEF9E1", fg="#000")
-result_limit_label.grid(row=0, column=2, padx=5)
-result_limit_entry = tk.Entry(button_frame, width=5, bg="#E5D0AC")
+# Configure the canvas
+canvas.configure(yscrollcommand=scrollbar.set)
+canvas.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+
+# Create a frame inside the canvas
+frame = ttk.Frame(canvas)
+canvas.create_window((0, 0), window=frame, anchor="nw")
+
+
+# Add title and subtitle
+tk.Label(root, image=image, bg="#FEF9E1").pack(pady=(30, 0))
+
+title_label = tk.Label(
+    root, text="Search Research", font=("Arial", 32, "bold"), bg="#FEF9E1", fg="#6D2323"
+)
+title_label.pack(pady=(0, 0))
+subtitle_label = tk.Label(
+    root,
+    text="Search for research papers with ease",
+    font=("Arial", 12),
+    bg="#FEF9E1",
+    fg="#000",
+)
+subtitle_label.pack(pady=(1, 10))
+ttk.Separator(root, orient="horizontal").pack(fill="x", padx=50, pady=(0, 10))
+
+# Search Actions and Label
+input_action_bar = ttk.Frame(root, style="Custom.TFrame")
+input_action_bar.pack(pady=(0, 10))
+tk.Label(
+    input_action_bar,
+    text="Enter your search query:",
+    bg="#FEF9E1",
+    fg="#6D2323",
+    font=("Arial", 12),
+).grid(row=0, column=1, padx=5)
+tk.Button(
+    input_action_bar,
+    text="Clear Input",
+    command=lambda: search_entry.delete(1.0, tk.END),
+    bg="#FEF9E1",
+    fg="#6D2323",
+    font=("Arial", 10, "bold"),
+).grid(row=0, column=0, padx=5)
+
+# Search Entry
+search_entry = scrolledtext.ScrolledText(
+    root, wrap=tk.WORD, height=5, width=70, bg="#E5D0AC"
+)
+search_entry.pack(pady=5)
+
+# Action Bar
+action_bar = ttk.Frame(root, style="Custom.TFrame")
+action_bar.pack(pady=10)
+tk.Button(
+    action_bar,
+    text="Clean",
+    command=clean_input,
+    bg="#6D2323",
+    fg="#FEF9E1",
+    font=("Arial", 10, "bold"),
+).grid(row=0, column=0, padx=5)
+tk.Button(
+    action_bar,
+    text="Deep Clean",
+    command=deep_clean_input,
+    bg="#6D2323",
+    fg="#FEF9E1",
+    font=("Arial", 10, "bold"),
+).grid(row=0, column=1, padx=5)
+result_limit_label = tk.Label(
+    action_bar, text="Results Limit:", bg="#FEF9E1", fg="#000"
+).grid(row=0, column=2, padx=5)
+result_limit_entry = tk.Entry(action_bar, width=5, bg="#E5D0AC")
 result_limit_entry.grid(row=0, column=3, padx=5)
 result_limit_entry.insert(0, "30")
-
-search_button = tk.Button(button_frame, text="Search", command=search_papers, bg="#6D2323", fg="#FEF9E1", font=("Arial", 10, "bold"))
+search_button = tk.Button(
+    action_bar,
+    text="Search",
+    command=search_papers,
+    bg="#6D2323",
+    fg="#FEF9E1",
+    font=("Arial", 10, "bold"),
+)
 search_button.grid(row=0, column=4, padx=5)
 
-clean_input_button = tk.Button(button_frame, text="Clean", command=clean_input, bg="#6D2323", fg="#FEF9E1", font=("Arial", 10, "bold"))
-clean_input_button.grid(row=0, column=5, padx=5)
+# Description
+tk.Label(
+    root,
+    text='"Clean" Removes special characters from the search query.',
+    font=("Arial", 10),
+    bg="#FEF9E1",
+    fg="#000",
+).pack(pady=(1, 5))
+tk.Label(
+    root,
+    text='"Deep Clean" Removes special characters, numbers, and multiple spaces from the search query.',
+    font=("Arial", 10),
+    bg="#FEF9E1",
+    fg="#000",
+).pack(pady=(1, 10))
+ttk.Separator(root, orient="horizontal").pack(fill="x", padx=50, pady=(0, 10))
 
-# Create the result field (a scrolled text widget)
-result_label = tk.Label(root, text="Search Results:", bg="#FEF9E1", fg="#000")
-result_label.pack(pady=5)
-result_field = scrolledtext.ScrolledText(root, wrap=tk.WORD, height=10, width=70, bg="#E5D0AC")
+# Output Actions and Label
+output_action_bar = ttk.Frame(root, style="Custom.TFrame")
+output_action_bar.pack(pady=(0, 10))
+tk.Label(
+    output_action_bar,
+    text="Search Results:",
+    bg="#FEF9E1",
+    fg="#6D2323",
+    font=("Arial", 12),
+).grid(row=0, column=1, padx=5)
+tk.Button(
+    output_action_bar,
+    text="Clear Output",
+    command=lambda: result_field.delete(1.0, tk.END),
+    bg="#FEF9E1",
+    fg="#6D2323",
+    font=("Arial", 10, "bold"),
+).grid(row=0, column=0, padx=5)
+
+
+result_field = scrolledtext.ScrolledText(
+    root, wrap=tk.WORD, height=10, width=70, bg="#E5D0AC"
+)
 result_field.pack(pady=5)
 
 
